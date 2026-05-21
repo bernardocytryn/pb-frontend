ae0f9d022580f8e24924bed9c011e1b1bdfa9e4c
todbernardo <bernardo.baims@gmail.com>
2026-05-21 09:30:10 -0300

    update na navbar e adicao de testes

--- ShapeCheck/src/components/layout/Navbar.jsx
+++ ShapeCheck/src/components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 import logo from "../../assets/logoshapecheck.png";
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({ nomeUsuario }) => {
   const estiloLink = ({ isActive }) =>
     isActive ? `${styles.item} ${styles.ativo}` : styles.item;
   return (
@@ -33,12 +33,20 @@ const Navbar = () => {
           </span>
           <span className={styles.texto}>Progresso</span>
         </NavLink>
-
+      </div>
+      <div className={styles.itensPerfil}>
         <NavLink className={estiloLink} to="/perfil">
           <span className={styles.icone}>
             <FaUserLarge />
           </span>
-          <span className={styles.texto}>Perfil</span>
+          <div className={styles.perfil}>
+            <span className={`${styles.texto} ${styles.textoOla}`}>
+              Olá, Bernardo{nomeUsuario}
+            </span>
+            <span className={styles.iconePerfil}>
+              <FaUserLarge />
+            </span>
+          </div>
         </NavLink>
       </div>
     </div>
---
+++ ShapeCheck/src/components/layout/Navbar.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import Navbar from "./Navbar";
+
+describe("Navbar", () => {
+  it("tem que renderizar os 4 navlinks pra navegar entre as paginas", () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+    const linkHome = screen.getByText("Home");
+    const linkTreinos = screen.getByText("Treinos");
+    const linkProgresso = screen.getByText("Progresso");
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkTreinos).toBeInTheDocument();
+    expect(linkProgresso).toBeInTheDocument();
+  });
+});
