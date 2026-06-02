5bde313fa4fab691790a9a010db7dd34cabfcf4b
Bernardo <bernardo.baims@gmail.com>
2026-06-01 22:16:35 -0300

    implementacao de gesto de swipe entre telas

--- ShapeCheck/src/App.jsx
+++ ShapeCheck/src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
-import "./App.css";
-import TelaBemvindo from "./pages/TelaBemvindo";
-import { Outlet, useLocation } from "react-router-dom";
+import { useLocation, useNavigate, Outlet } from "react-router-dom";
+import { useSwipeable } from "react-swipeable";
 import Navbar from "./components/layout/Navbar";
+import "./App.css";
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const ordemDasTelas = ["/home", "/treino", "/dashboard", "/perfil"];
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      const indexAtual = ordemDasTelas.indexOf(location.pathname);
+      if (indexAtual !== -1 && indexAtual < ordemDasTelas.length - 1) {
+        navigate(ordemDasTelas[indexAtual + 1]);
+      }
+    },
+    onSwipedRight: () => {
+      const indexAtual = ordemDasTelas.indexOf(location.pathname);
+      if (indexAtual > 0) {
+        navigate(ordemDasTelas[indexAtual - 1]);
+      }
+    },
+    preventScrollOnSwipe: false,
+    trackTouch: true,
+    trackMouse: false,
+  });
+
+  const mostrarNavbar =
+    location.pathname !== "/" && location.pathname !== "/wizard";
 
   return (
-    <>
-      <main>
-        {location.pathname !== "/" && location.pathname !== "/wizard"  && (
-          <Navbar />
-        )}
-        <Outlet />
-      </main>
-    </>
+    <main
+      {...handlers}
+      style={{ minHeight: "100vh", width: "100%", overflowX: "hidden" }}
+    >
+      {mostrarNavbar && <Navbar />}
+
+      <Outlet />
+    </main>
   );
 }
 
