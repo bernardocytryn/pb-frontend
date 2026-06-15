80f5fe79e9070cc09e6ce02d9cae65e4e6c7bfe5
Bernardo <bernardo.baims@gmail.com>
2026-06-15 02:07:12 -0300

    att navbar

--- ShapeCheck/src/components/layout/Navbar.jsx
+++ ShapeCheck/src/components/layout/Navbar.jsx
@@ -3,17 +3,33 @@ import { IoMdHome } from "react-icons/io";
 import { IoBarbell } from "react-icons/io5";
 import { IoStatsChartSharp } from "react-icons/io5";
 import { FaUserLarge } from "react-icons/fa6";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/logoshapecheck.png";
 import styles from "./Navbar.module.css";
 
 const Navbar = ({ nomeUsuario }) => {
+  const location = useLocation();
+
   const estiloLink = ({ isActive }) =>
     isActive ? `${styles.item} ${styles.ativo}` : styles.item;
+
+  const rotasDeTreino = [
+    "/treino",
+    "/treinos",
+    "/series",
+    "/consultar",
+    "/criar-serie",
+  ];
+
+  const isTreinoAtivo = rotasDeTreino.some((rota) =>
+    location.pathname.startsWith(rota),
+  );
+
   return (
     <div className={styles.navbar}>
       <div className={styles.itens}>
         <img src={logo} alt="logo" />
+
         <NavLink className={estiloLink} to="/home">
           <span className={styles.icone}>
             <IoMdHome />
@@ -21,7 +37,12 @@ const Navbar = ({ nomeUsuario }) => {
           <span className={styles.texto}>Home</span>
         </NavLink>
 
-        <NavLink className={estiloLink} to="/treino">
+        <NavLink
+          className={() =>
+            isTreinoAtivo ? `${styles.item} ${styles.ativo}` : styles.item
+          }
+          to="/treino"
+        >
           <span className={styles.icone}>
             <IoBarbell />
           </span>
@@ -35,6 +56,7 @@ const Navbar = ({ nomeUsuario }) => {
           <span className={styles.texto}>Dashboard</span>
         </NavLink>
       </div>
+
       <div className={styles.itensPerfil}>
         <NavLink className={estiloLink} to="/perfil">
           <span className={styles.icone}>
