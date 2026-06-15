54f9037e3fb8de60f55c4e70e9c7d02766702248
Daniel Lages <danielperalta98@gmail.com>
2026-06-15 04:34:01 -0300

    cards e layout tela Dashboard

---
+++ ShapeCheck/src/features/dashboard/GraficoKcal.jsx
@@ -0,0 +1,22 @@
+import { LineChart } from '@mui/x-charts/LineChart';
+
+
+function GraficoKcal() {
+
+   return (
+      <div>
+         <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+               {
+                  data: [2, 5.5, 2, 8.5, 1.5, 5],
+                  area: true,
+               },
+            ]}
+            height={300}
+         />
+      </div>
+   );
+}
+
+export default GraficoKcal;
